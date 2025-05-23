557df61f2f954cd3046e6d489ca06b04be38a5b6
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2025-05-23 13:39:29 -0600

    reseting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
 // src/app/page.tsx
-import Hero from "@/components/home/Hero";
-import VehicleSection from "@/components/home/VehicleSection";
-import CategoriesSection from "@/components/home/CategoriesSection";
-import UniversalItemsSection from "@/components/home/UniversalItemsSection";
 
 export default function HomePage() {
-  return (
-    <main className="min-h-screen">
-      {/* Hero Section with Search */}
-      <Hero />
-
-      {/* Vehicle Selection Section */}
-      <VehicleSection />
-
-      {/* Categories Section for Parts */}
-      <CategoriesSection />
-
-      {/* Universal Items Section (Clothing, Helmets, Accessories) */}
-      <UniversalItemsSection />
-    </main>
-  );
+  return <main className="min-h-screen"></main>;
 }
